e9ac87020410f23140c737e5117e0f9a5762f92b
VishiK-on-github <vck281000@gmail.com>
2020-10-09 15:03:58 +0530

    additional functions for validation of source destination and weights added

--- static/validate.js
+++ static/validate.js
@@ -19,37 +19,117 @@ function validateForm() {
         var getDestination = document.getElementsByName("destination[]");
         var getWeight = document.getElementsByName("weight[]");
 
-        var sourceLength = getSource.length;
-        var destinationLength = getDestination.length;
-        var weightLength = getWeight.length;
-
         // Validation of form left
 
         /*alert(`Source array length : ${sourceLength}`);
         alert(`Destination array length : ${destinationLength}`);
         alert(`weight array length : ${weightLength}`);*/
 
-        if((sourceLength <= limit_edge)&&(destinationLength <= limit_edge)&&(weightLength <= limit_edge)) {
+        if((getSource.length <= limit_edge)&&(getDestination.length <= limit_edge)&&(getWeight.length <= limit_edge)) {
 
-            for(var i=0; i<= sourceLength; i++) {
+            function validFormStatus() {
 
-                if(Number.isInteger(getSource[i].value)) {
-
-                    getSource[i].style.border = "2px solid lightgreen";
-                }
-            }
+                // Container method which determines validity of the form
+    
+                sf1 = validSourceDestination(getSource, getDestination, number_node);
+                sf2 = validEdgeWeight(getWeight);
+    
+                // Returning the form status
+                return (sf1 && sf2);
+            } 
+    
+            return validFormStatus();
         }
         else {
 
-            alert("Lengths dont match !");
+            // This condition handles if edges exceed limit of the graph
+
+            alert("Please remove some edges. The provided set of edges may result in an invalid graph !");
             return false;
         }
     }
     else {
 
+        // This condition handles if number of nodes fields is empty
+
         alert("Please give number of nodes !");
         return false;
     }
+}
 
-    return false;
+function validSourceDestination(sourceArr, destinationArr, maxVal) {
+
+    // Method to check if valid source and destination have been entered
+
+    var srcDestFlagStatus = true;
+
+    for(var i=0; i < sourceArr.length; i++) {
+
+        if(sourceArr[i].value != destinationArr[i].value) {
+
+            if((Number.isInteger(parseInt(sourceArr[i].value)))&&(parseInt(sourceArr[i].value) < maxVal)) {
+
+                sourceArr[i].style.border = "2px solid lightgreen";
+            }
+            else {
+
+                sourceArr[i].style.border = "2px solid red";
+                if (srcDestFlagStatus == true) {
+
+                    srcDestFlagStatus = false;
+                }
+            }
+            if((Number.isInteger(parseInt(destinationArr[i].value)))&&(parseInt(destinationArr[i].value) < maxVal)) {
+
+                destinationArr[i].style.border = "2px solid lightgreen";
+            }
+            else {
+
+                destinationArr[i].style.border = "2px solid red";
+                if (srcDestFlagStatus == true) {
+
+                    srcDestFlagStatus = false;
+                }
+            }
+        }
+        else {
+
+            sourceArr[i].style.border = "2px solid red";
+            destinationArr[i].style.border = "2px solid red";
+            if (srcDestFlagStatus == true) {
+
+                srcDestFlagStatus = false;
+            }
+        }
+    }
+
+    // returning the status of the source and destination input fields
+    return srcDestFlagStatus;
+}
+
+
+function validEdgeWeight(weightArr) {
+
+    // Method to check if valid weights have been entered
+
+    var weightFlagStatus = true;
+
+    for(var i=0; i < weightArr.length; i++) {
+
+        if((weightArr[i].value == "infinity")||(Number.isInteger(weightArr[i].value))) {
+
+            weightArr[i].style.border = "2px solid lightgreen";
+        }
+        else {
+
+            weightArr[i].style.border = "2px solid red";
+            if (weightFlagStatus == true) {
+
+                weightFlagStatus = false;
+            }
+        }
+    }
+
+    // returning the status of the weight input field
+    return weightFlagStatus;
 }
