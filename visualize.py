d2b05be72e9e612794e23481c9b9b51dc4a7b5c0
VishiK-on-github <vck281000@gmail.com>
2020-10-12 17:34:47 +0530

    added some comments

--- visualize.py
+++ visualize.py
@@ -11,10 +11,10 @@ import math
 def build_graph(n, adjMatrix, location):
     
     # Receieving number of vertices, adjacency matrix and location of where image has to be stored
-    # To generate graph with random vertices
+    # To generate graph with random vertices layout
     G = nx.lollipop_graph(n, 0)
 
-    # Creating a direct graph
+    # Creating a directed graph
     G = nx.DiGraph()
 
     #adjMatrix = np.array([[math.inf, -5, 2, 3], [math.inf, math.inf, 4, math.inf], [math.inf, math.inf, math.inf, 1], [math.inf, math.inf, math.inf, math.inf]])
@@ -23,10 +23,12 @@ def build_graph(n, adjMatrix, location):
     # Adding edges to graph
     for i in range(length):
         for j in range(length):
-            if(adjMatrix[i,j]==math.inf):
+
+            if(adjMatrix[i,j] == math.inf):
                 continue
+
             G.add_edge(i, j, weight=adjMatrix[i, j])
-            print(adjMatrix[i, j])
+            #print(adjMatrix[i, j])
 
     # Retrieving position of nodes
     pos = nx.random_layout(G)
--- visualizeDijkstra.py
+++ visualizeDijkstra.py
@@ -1,24 +1,35 @@
 """Code to visualize Djikstra Table form output"""
 
+# Importing dependencies
 import visualize as vis
 import numpy as np 
 import math
 
 
-#Using this function to create adjacency matrices of shortest paths from all vertices(as source) returned by main JohnsonsAlgorithm program
-def convert(mainTable,iteration):
-    num=len(mainTable)
-    adjMatrix = np.full([num, num], math.inf)
-    #Creating adjacency matrix from main table returned by JohnsonsAlgorithm program
-    for j,i  in enumerate(mainTable):
-        parent=i[1]
-        distance=i[0]
-        if parent==None:
-            continue
-        adjMatrix[parent,j]=distance-mainTable[parent][0]
-    print(mainTable)
-    print(adjMatrix)
+def convert(mainTable, iteration):
 
-    #Sending to adjacency matric to visualize.py to create/save graphs as per the iteration
+   """Using this function to create adjacency matrices of shortest 
+   paths from all vertices (as source) returned by main JohnsonsAlgorithm program"""
+
+    num = len(mainTable)
+    adjMatrix = np.full([num, num], math.inf)
+
+    # Since we send the table (mainTable) a table which consists of 
+    # solution for the computed graph using Dijkstras
+    # Creating adjacency matrix from main table returned by JohnsonsAlgorithm program
+    for j, i  in enumerate(mainTable):
+
+        parent = i[1]
+        distance = i[0]
+
+        if parent == None:
+            continue
+
+        adjMatrix[parent, j] = distance - mainTable[parent][0]
+
+    #print(mainTable)
+    #print(adjMatrix)
+
+    # Sending to adjacency matric to visualize.py to create/save graphs as per the iteration
     location="../Johnsons-Algorithm/static/images/soln{0}.png".format(iteration)
     vis.build_graph(num,adjMatrix,location)
